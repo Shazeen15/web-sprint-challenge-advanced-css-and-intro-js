43c098ee944c1857a316c3878f767eea79ead014
Shazeen A Fabius <Shazeen-Fabius@Shazeen-MBP.lan>
2020-09-11 08:57:33 -0700

    completed up to task 5

--- index.js
+++ index.js
@@ -207,11 +207,13 @@ const artists = [
 
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
-
+console.log(artists[0]);
+console.log(artists[2]);
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
+artists[8].name = 'Vincent Van Gogh';
+console.log(artists[8].name)
 
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
@@ -224,19 +226,30 @@ const artists = [
 */
 function getArtistByIndex(array, index) {
     /* code here */
-    
+    let name = artists[index].name;
+    return `Artist at index ${index} is ${name}`
   }
-  
+  console.log(getArtistByIndex(artists, 0));
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
+function get20s(year){
+  let twenties = [];
 
+  for (let i = 0; i < artists.length; i++){
+    // if(artists[i].years >= year && reviews[i].years <= year){
+    //   twenties.push(reviews[i].name);
+    // }//if
+    if(artists[i].years.includes(year)){
+      twenties.push(artists[i].name);
+    }//if
+  }//for
+  return twenties;
   /* Code here */
-
 }
+console.log(get20s('190'))
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
@@ -249,10 +262,15 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
+function removeArtist(array, indexNum) {
     /* code here */
+    artists.splice(indexNum, 1); //remove 1 element at w.e index the flavor that was chosen in on
+    return array;
+
   }
-  
+  console.log(removeArtist(artists, 1));
+  // console.log(removeArtist(artists, 1));
+  // console.log(removeArtist(artists, 1));
  
 
 /**
